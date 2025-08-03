d5d9a23df2080c6dcfa59d1fe178394397e3c164
vkopcha <BigV@MacBook-Air.local>
2025-08-03 23:42:53 +0100

    create a base visual

--- src/app/components/MainPage/MainPage.tsx
+++ src/app/components/MainPage/MainPage.tsx
@@ -1,11 +1,40 @@
 import {MainPageConfig} from "@/types/main";
+import {SearchComponent} from "@/app/components/Search/SearchComponent";
+import {ItemsComponent} from "@/app/ItemsComponent/ItemsComponent";
 
 interface MainPageProps {
     config: MainPageConfig;
 }
 
+const products = [
+    {
+        image: "https://via.placeholder.com/300x200?text=Modern+Plates",
+        title: "Product 1",
+    },
+    {
+        image: "https://via.placeholder.com/300x200",
+        title: "Product 2",
+    },
+    {
+        image: "https://via.placeholder.com/300x200",
+        title: "Product 3",
+    },
+];
 export const MainPage  = ({config}: MainPageProps) => {
     return (
-        <div className="flex flex-col gap-3 p-4">{config.lible}</div>
+        <div className="flex flex-col gap-3 h-screen">
+            <div className="px-5 py-3 shrink-0"><SearchComponent/></div>
+                <div className="flex-1 overflow-y-auto grid grid-cols-2 md:grid-cols-3 gap-4 p-4">
+                    {products.map((product, index) => (
+                        <ItemsComponent
+                            key={index}
+                            image={product.image}
+                            title={product.title}
+                        />
+                    ))}
+
+                </div>
+        </div>
+
     )
 }
